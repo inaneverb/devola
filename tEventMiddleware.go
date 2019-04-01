2799d29680c6232709671897861d83fca0cfbdaf
Alice Qio <qioalice@gmail.com>
2019-04-01 04:46:32 +0300

    Added EventType, EventHandler, EventMiddleware types
    Added some basic types as parts of handling Telegram Bot Updates.
    EventType is the type of received Telegram Bot event: Command, Text, Button, etc.
    EventHandler is the handler of occurred event.
    EventMiddleware is the pre-handler of occurred event and its regulator.

---
+++ tEventMiddleware.go
@@ -0,0 +1,48 @@
+// Copyright Â© 2018. All rights reserved.
+// Author: Alice Qio.
+// Contacts: <qioalice@gmail.com>.
+// License: https://opensource.org/licenses/MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package tgbot
+
+// tEventMiddleware is the function, callback, that registered by you as
+// a function for some event and it will be called when that event is occurred,
+// but BEFORE event handler (tHandlerCallback) calling!
+//
+// Moreover, your function, a middleware, should return true or false.
+// It's an indicator.
+// If event handler callback(s) must be called and event
+// should be proceed, you must return true from ALL registered middleewares.
+// If at least one middleware returns false, the rest of event middlewares
+// and event handlers will never called for occurred event and event
+// will be discarded.
+//
+// It guarantees, that when the middlewares is called, the TCtx object
+// is considered completely created and TCtx object will be passed to the
+// handler callback in the form in which it is after middleware call
+// will finish.
+// So, it means all changes in TCtx object made inside middleware will be saved
+// and handler callback will receive modified by you TCtx object.
+// But there is no internal modifies of TCtx object between middleware
+// and handler calls.
+//
+// More info: tReceiver, TCtx, tBehaviourCreator, TBot.
+type tEventMiddleware func(c *Ctx) bool
---
+++ tEventType.go
@@ -0,0 +1,76 @@
+// Copyright Â© 2018. All rights reserved.
+// Author: Alice Qio.
+// Contacts: <qioalice@gmail.com>.
+// License: https://opensource.org/licenses/MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package tgbot
+
+// tEventType represents type of event that is occurred.
+// This is a part of tEvent object, which is a part of TCtx.
+//
+// Thus you can always to figure out inside handler which kind of event
+// your handler handling. Use predefined constants, presented below for this.
+//
+// More info: tEvent, TCtx.
+type tEventType uint8
+
+// Constants of tEventType.
+// Use these constants to figure out what kind of event is occurred
+// (by comparing tEventType).
+const (
+	// Internal type.
+	// A marked invalid type.
+	cEventTypeInvalid tEventType = 0 + iota
+
+	// Internal type.
+	// A not fully determined type,
+	// but it is either keyboard button type or text type.
+	cEventTypeKeyboardButtonOrText tEventType = 2
+
+	// A chat text command.
+	// tEvent's Data field represents a lowercase command without arguments.
+	CEventTypeCommand tEventType = 100
+
+	// A pressed keyboard button.
+	// Technically this is a text (in a chat), but a text sent by
+	// pressing to the keyboard button.
+	// tEvent's Data field represents this keyboard button data.
+	CEventTypeKeyboardButton tEventType = 101
+
+	// A typed text.
+	// tEvent's Data field represents the whole text but with trimmed
+	// leading and trailing spaces.
+	CEventTypeText tEventType = 102
+
+	// A pressed inline keyboard button.
+	// tEvent's Data field stored TAction value, representing the your
+	// action causes occured event.
+	CEventTypeInlineKeyboardButton tEventType = 200
+)
+
+// isValid returns true only if ht is public tEventType predefined constant.
+// Otherwise false is returned.
+func (et tEventType) isValid() bool {
+	return et == CEventTypeCommand ||
+		et == CEventTypeKeyboardButton ||
+		et == CEventTypeText ||
+		et == CEventTypeInlineKeyboardButton
+}
