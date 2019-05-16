b6eddd87b3ec6e6e385eb2c50bbec418d7b25d9e
Alice Qio <qioalice@gmail.com>
2019-05-16 15:16:13 +0300

    Refactored errors package
    Added comments and docs.
    Added Is, Is2 functions - error comparators.
    Added public default SDK error interface type.
    Added public default SDK error base type.

--- internal/errors/code.go
+++ internal/errors/code.go
@@ -5,5 +5,16 @@
 
 package errors
 
-// todo: comment
+// Code represents error code type.
+// The values of this type are returned by method Code of each type that
+// implements Error interface, and also sometimes are returned directly
+// from methods.
 type Code int
+
+// Predefined constants of error codes.
+// First two letters of each consts, EC, means Error Code.
+const (
+
+	// ECOK means Error Code OK: Success, No errors.
+	ECOK Code = 0
+)
